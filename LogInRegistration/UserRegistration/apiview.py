ab9940ea87ee7cf99a2c184532b37fdae6df3a29
santospanda111 <santospanda111@gmail.com>
2021-08-17 22:22:26 +0530

    Modified NoteApi View

--- LogInRegistration/UserRegistration/apiview.py
+++ LogInRegistration/UserRegistration/apiview.py
@@ -1,6 +1,11 @@
 from UserRegistration.views import Index,LogIn,Register
 from kwikapi import API
 
+"""
+    Here i have created an Api named user_api and registered Index with namespace = home,
+    Register with namespace = register,LogIn with namespace = login
+"""
+
 user_api=API()
 user_api.register(Index(),'home')
 user_api.register(Register(),'register')
